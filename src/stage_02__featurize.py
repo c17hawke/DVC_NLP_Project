7224a52c634f6e3c94807c248f1e3d882b26942b
sushantsur23 <sushant.sur23@gmail.com>
2022-06-19 23:03:01 +0530

    stage 2 completed

--- src/stage_02__featurize.py
+++ src/stage_02__featurize.py
@@ -1,13 +1,13 @@
 import argparse
+import numpy as np
 import os
-import shutil
-from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils import read_yaml, create_directories, save_matrix, get_df
 import random
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "Featurization" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -21,7 +21,50 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    artifacts = config["artifacts"]
+    prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+
+    train_data_path =  os.path.join(prepare_data_dir_path, artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepare_data_dir_path, artifacts["TEST_DATA"])
+
+    featurized_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    create_directories([featurized_data_dir_path])
+
+    featurized_train_data_path = os.path.join(featurized_data_dir_path,artifacts["FEATURIZED_DATA_TRAIN"])
+    featurized_test_data_path = os.path.join(featurized_data_dir_path,artifacts["FEATURIZED_DATA_TEST"])  
+
+    max_features = params["featurize"]["max_features"]
+    n_grams = params["featurize"]["n_grams"]
+
+    #for train data
+    df_train = get_df(train_data_path)
+    train_words = np.array(df_train.text.str.lower().values.astype("U"))
+
+    bag_of_words = CountVectorizer(
+        stop_words ="english",
+        max_features = max_features,
+        ngram_range = (1,n_grams)
+    )
+
+    bag_of_words.fit(train_words)
+    train_words_binary_matrix = bag_of_words.transform(train_words)
+
+    tfidf = TfidfTransformer(smooth_idf = False)
+    tfidf.fit(train_words_binary_matrix)
+    train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
+
+    #call a function to save the matrix
+
+    #for test data
+    df_test = get_df(test_data_path)
+    test_words = np.array(df_test.text.str.lower().values.astype("U"))
+
+    test_words_binary_matrix = bag_of_words.transform(test_words)
+
+    test_words_tfidf_matrix = tfidf.transform(test_words_binary_matrix)
+
+    #call a function to save the matrix
 
 
 if __name__ == '__main__':
